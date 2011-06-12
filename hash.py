e184b00316422f545ac3197a540b02e7559817eb
System Administrator <jmnwong@gmail.com>
2011-06-12 15:02:03 -0400

    Hash updated and added face detection

--- hash.py
+++ hash.py
@@ -1,17 +1,6 @@
 import Image
 import os, sys
 
-im = Image.open("portal2.jpg")
-imlarger = im.copy();
-im = im.resize((8, 8))
-imlarger = imlarger.resize((32,32))
-im = im.convert("L")
-imlarger = imlarger.convert("L")
-print "\nResolution:\t%s" % (im.size,)
-print "Mode:\t\t" + im.mode
-im.save("foo.jpg")
-imlarger.save("foo2.jpg")
-
 # find the average value
 def average(image):
 	count = 0
@@ -19,24 +8,23 @@ def average(image):
 	for i in range(0,8):
 		for j in range(0,8):
 			count += pix[i, j]
-	after = count / 64
+	val = count / 64
 	print "Total:\t\t%d" % count
-	print "Average:\t%d" % after
-	return after
+	print "Average:\t%d" % val
+	return val
 
-# computes the hamming distance
-def hamming_distance(s1, s2):
-	assert len(s1) == len(s2)
-	return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+# find the DCT value
+def dct(image):
+	mean = average(image)	
+	compute(image, mean, 32)
+	print "Average:\t%d" % mean
 
 # compute the hash
-def compute(image, av):
+def compute(image, av, size):
 	testString = ""
-	for i in range(0,8):
-		for j in range(0,8):
+	for i in range(0,size):
+		for j in range(0,size):
 			pix = image.load()
-			#print "--------------"
-			#print "(%d,%d): %d" % (i,j, pix[i,j])
 			if(pix[i, j] >= av):
 				pix[i, j] = 1
 			else:
@@ -46,6 +34,22 @@ def compute(image, av):
 	encoded = "%x" % int(testString,2)
 	print "Hash:\t\t" + encoded + "\n"
 	
+# computes the hamming distance
+def hamming_distance(s1, s2):
+	assert len(s1) == len(s2)
+	return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
 # printing things out
-avVal = average(im)
-compute(im, avVal)
+def main():
+	im = Image.open(sys.argv[1])
+	im = im.resize((32, 32))
+	im = im.convert("L")
+	print "\nResolution:\t%s" % (im.size,)
+	print "Mode:\t\t" + im.mode
+	im.save("output.jpg")
+	avVal = average(im)
+	compute(im, avVal, 8)
+	#dct(im)
+
+if __name__ == "__main__":
+	main()
